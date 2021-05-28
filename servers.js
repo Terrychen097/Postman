a4d9e4044edb26c31dfa84b3bf44d75efeab6406
Terry Chen <terry470343312@gmail.com>
2021-05-28 17:47:58 -0400

    make it better

--- servers.js
+++ servers.js
@@ -14,6 +14,8 @@ const HTTP_PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 // Deliver the app's home page to browser clients
+
+
 app.post("/api/restaurant", (req, res) => {
   db.addNewRestaurant(req.body)
   .then(msg=>res.json(msg))
